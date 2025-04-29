f682a386b358748526721a37eb44cc9442f1def8
MetronARC <ricoroenaldo@metronarc.com>
2025-04-29 21:45:59 +0700

    new database

--- database/db_rigwise.sql
+++ database/db_rigwise.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 29, 2025 at 01:28 PM
+-- Generation Time: Apr 29, 2025 at 02:45 PM
 -- Server version: 9.1.0
 -- PHP Version: 8.3.14
 
@@ -42,31 +42,31 @@ CREATE TABLE IF NOT EXISTS `rigwise_carousel` (
 
 INSERT INTO `rigwise_carousel` (`ID`, `carousel_image`, `carousel_alt`, `carousel_title`) VALUES
 (1, '10001.JPG', 'Marine Operation', 'Joining'),
-(2, '10002P.JPG', 'Marine Operation', 'Preparing for Move'),
-(3, '10003P.JPG', 'Marine Operation', 'Inspecting Tugs and AHTS for Safety'),
-(4, '10004P.JPG', 'Marine Operation', 'Testing Emergency Release'),
+(2, '10002p.JPG', 'Marine Operation', 'Preparing for Move'),
+(3, '10003p.JPG', 'Marine Operation', 'Inspecting Tugs and AHTS for Safety'),
+(4, '10004p.JPG', 'Marine Operation', 'Testing Emergency Release'),
 (5, '10005.JPG', 'Marine Operation', 'Maneuvering to Connect'),
 (6, '10006P.JPG', 'Marine Operation', 'Maneuvering to Connect'),
-(7, '10007S.JPG', 'Marine Operation', 'Billy Pugh Transfer'),
+(7, '10007s.JPG', 'Marine Operation', 'Billy Pugh Transfer'),
 (8, '10008S.JPG', 'Marine Operation', 'Lead AHTS Presenting for Connection'),
 (9, '10009.JPG', 'Marine Operation', 'Releasing the Tow Bridle'),
-(10, '10010S.JPG', 'Marine Operation', 'Catching the Tow Bridle'),
-(11, '10011PS.JPG', 'Marine Operation', 'AHTS Preparing to Connect, Tugger Wire Passes Through Towing Pins, Main Tow Prepared'),
+(10, '10010s.JPG', 'Marine Operation', 'Catching the Tow Bridle'),
+(11, '10011ps.JPG', 'Marine Operation', 'AHTS Preparing to Connect, Tugger Wire Passes Through Towing Pins, Main Tow Prepared'),
 (12, '10012.JPG', 'Marine Operation', 'Connecting Main Tow'),
-(13, '10013PS.JPG', 'Marine Operation', 'Main Tow Connected, Preparing to Tow'),
-(14, '10014PS.JPG', 'Marine Operation', 'Main Tow Connected'),
-(15, '10015S.JPG', 'Marine Operation', 'Main Tow Connected'),
-(16, '10016S.JPG', 'Marine Operation', 'Main Tow Connected and Checked'),
+(13, '10013ps.JPG', 'Marine Operation', 'Main Tow Connected, Preparing to Tow'),
+(14, '10014ps.JPG', 'Marine Operation', 'Main Tow Connected'),
+(15, '10015s.JPG', 'Marine Operation', 'Main Tow Connected'),
+(16, '10016s.JPG', 'Marine Operation', 'Main Tow Connected and Checked'),
 (17, '10017.JPG', 'Marine Operation', 'Stretching the Tow'),
-(18, '10018S.JPG', 'Marine Operation', 'Moving into Position'),
-(19, '10019SP.JPG', 'Marine Operation', 'Paying out Tow Wire, Moving into Position'),
-(20, '10020S.JPG', 'Marine Operation', 'Paying out Tow Wire, Moving into Position'),
-(21, '10021S.JPG', 'Marine Operation', 'Connecting Secondary Tugs'),
+(18, '10018s.JPG', 'Marine Operation', 'Moving into Position'),
+(19, '10019Ssp.JPG', 'Marine Operation', 'Paying out Tow Wire, Moving into Position'),
+(20, '10020s.JPG', 'Marine Operation', 'Paying out Tow Wire, Moving into Position'),
+(21, '10021s.JPG', 'Marine Operation', 'Connecting Secondary Tugs'),
 (22, '10022p.JPG', 'Marine Operation', 'Underway and Making Way'),
-(23, '10023PS.JPG', 'Marine Operation', 'Checking Tugs and Tow'),
+(23, '10023ps.JPG', 'Marine Operation', 'Checking Tugs and Tow'),
 (24, '10024sp.JPG', 'Marine Operation', 'Maintaining Course and Speed'),
-(25, '10025S.JPG', 'Marine Operation', 'Preparing for the Night'),
-(26, '10026S.JPG', 'Marine Operation', 'Sunset'),
+(25, '10025s.JPG', 'Marine Operation', 'Preparing for the Night'),
+(26, '10026s.JPG', 'Marine Operation', 'Sunset'),
 (27, '10027ps.JPG', 'Marine Operation', 'Morning Pre-Arrival Checks'),
 (28, '10028sp.JPG', 'Marine Operation', 'Preparing for Arrival'),
 (29, '10029.JPG', 'Marine Operation', 'Checking Final Positioning'),
